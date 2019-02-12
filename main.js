a6ef9f7671c0ffc670a033d6a5c27e99f1c279f9
Brittany Barnes <brittanybarnes@Maddies-Mac-mini.local>
2019-02-12 13:59:03 -0500

    components in components

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+Vue.component('task-list', {
+	template: 
+	`
+		<div>
+			<task v-for="task in tasks">{{ task.task }}</task>
+		</div>
+	`,
+
+	data() {
+		return {
+			tasks: [
+
+			{ task: 'Go to the store', complete:true },
+			{ task: 'Go to the mall', complete:true },
+			{ task: 'Go to work', complete:false },
+			{ task: 'Go to pharmacy', complete:false },
+
+
+			]
+		}
+	}
+});
+
+
 Vue.component('task', {
 	template: '<li><slot></slot></li>'
 });
