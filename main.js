01431bec89987099c38f8b4d323060d5e810b74c
Brittany Barnes <brittanybarnes@Maddies-Mac-mini.local>
2019-02-18 14:36:11 -0500

    component communication

--- main.js
+++ main.js
@@ -1,79 +1,25 @@
-// added an extra wrapping div because we need one parent element, not two siblings for compnents to work
-Vue.component('tabs', {
-	template: 
-	`
-	<div>
-		<div class="tabs">
-  			<ul>
-  			  <li v-for="tab in tabs" :class="{ 'is-active' : tab.isActive }">
+Vue.component('coupon', {
+	template: '<input placeholder="enter coupon code" @blur="onCouponApplied">',
 
-  			  <a :href="tab.href" @click="selectTab(tab)">{{tab.name}}</a>
-
-  			  </li>
-  			</ul>
-		</div>
-
-		<div class="tabs-details">
-
-			<slot></slot>
-
-		</div>
-	</div>
-	`,
-
-	//be clear on what data the component exposes
-	data() {
-		return { tabs:[] };
-	},
-
-	// when this component is created, make tabs and make it equal to the tab children
-	created() {
-		this.tabs = this.$children;
-	},
-
-	//define any methods that the component needs
 	methods: {
-		selectTab(selectedTab) {
-			this.tabs.forEach( tab => {
-				tab.isActive = (tab.name == selectedTab.name);
-			})
+		onCouponApplied() {
+			this.$emit('applied');
 		}
 	}
-});
-
-Vue.component('tab', {
-	template: `
-
-		<div v-show="isActive"><slot></slot></div>
-
-	`,
-	// bring in the name from the html to be used in this 
-	// properties are immutable
-	props: {
-		name: { required: true },
-		selected: { default: false }
-	},
-
-	data() {
-		return {
-			isActive: false,
-		}
-		
-	},
-
-	computed: {
-		href() {
-			return '#' + this.name.toLowerCase().replace(/ /g, '-');
-		}
-	},
-
-	mounted() {
-		this.isActive = this.selected;
-	}
-
 });
 
 new Vue({
 	el: '#root',
 
+	data: {
+		couponApplied: false
+	},
+
+	methods: {
+		onCouponApplied() {
+			this.couponApplied = true;
+		}
+	}
+
 });
+
